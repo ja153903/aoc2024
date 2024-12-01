7e8b150e898e494f9f883a6d6811a0f33fdfca96
ja153903 <jaimeabbariao@gmail.com>
2024-11-30 22:50:31 -0500

    initial commit

---
+++ src/day01/main.rs
@@ -0,0 +1,26 @@
+pub fn read_input() -> String {
+    String::from(include_str!("./data.in"))
+}
+
+pub fn part1() -> i32 {
+    todo!("Implement part 1")
+}
+
+pub fn part2() -> i32 {
+    todo!("Implement part 2")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{part1, part2};
+    
+    #[test]
+    pub fn test_part1() {
+        assert_eq!(part1(), 0);
+    }
+
+    #[test]
+    pub fn test_part2() {
+        assert_eq!(part2(), 0);
+    }
+}
